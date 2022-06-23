4e4366f1db18c6d7190a75a5d549465c3024f397
marywang@usc.edu <marywang@usc.edu>
2022-06-23 10:08:03 -0700

    trying server again

--- server/routes/user.js
+++ server/routes/user.js
@@ -17,6 +17,31 @@ userRoutes.route('/user/add').post(function (req, response) {
   });
 });
 
+const allowCors = (fn) => async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // another common pattern
+  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
+  );
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+  return await fn(req, res);
+};
+
+const handler = (req, res) => {
+  const d = new Date();
+  res.end(d.toString());
+};
+
 // userRoutes.route('/update/:userId').post(function (req, response) {
 //   let db_connect = dbo.getDb();
 //   let myquery = { userId: req.body.userId };
@@ -27,4 +52,7 @@ userRoutes.route('/user/add').post(function (req, response) {
 //   };
 // });
 
-module.exports = userRoutes;
+module.exports = {
+  first: userRoutes,
+  second: allowCors(handler),
+};
