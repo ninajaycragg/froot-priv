dfded5b8304591eb0313c2a39e5b398164a5355d
marywang@usc.edu <marywang@usc.edu>
2022-06-24 12:15:13 -0700

    fixing next.config

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ module.exports = {
     return [
       {
         // matching all API routes
-        source: '/user/add',
+        source: '/api/hello',
         headers: [
           { key: 'Access-Control-Allow-Credentials', value: 'true' },
           { key: 'Access-Control-Allow-Origin', value: '*' },
