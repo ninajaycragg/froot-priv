8a189a54e994a35341f17a95b432ef4160275192
Anthony Martino <martinoa@usc.edu>
2022-11-03 14:25:33 -0700

    testing api

--- pages/login.js
+++ pages/login.js
@@ -26,7 +26,7 @@ export default function Login() {
             password: inputs["password"],
         };
 
-        fetch('user/loginUser', {
+        fetch('https://data.mongodb-api.com/app/data-fhwnd/endpoint/data/v1/user/loginUser', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
@@ -51,7 +51,7 @@ export default function Login() {
 
     useEffect(() => {
         // console.log(`useeffect: ${localStorage.getItem('token')}`);
-        fetch('user/auth', {
+        fetch('https://data.mongodb-api.com/app/data-fhwnd/endpoint/data/v1/user/auth', {
             headers: {
                 'Authorization': localStorage.getItem('token')
             }
